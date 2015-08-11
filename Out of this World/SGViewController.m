f7d944b83ac7e5c847fe351a321685f839be2c41
Jason Williams <jason@Jasons-MacBook-Pro.local>
2015-08-11 02:04:52 -0230

    Initial Commit

---
+++ Out of this World/SGViewController.m
@@ -0,0 +1,29 @@
+//
+//  SGViewController.m
+//  Out of this World
+//
+//  Created by Jason Williams on 2015-08-11.
+//  Copyright (c) 2015 Screaming Goat. All rights reserved.
+//
+
+#import "SGViewController.h"
+
+@interface SGViewController ()
+
+@end
+
+@implementation SGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Out of this World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of this World
+//
+//  Created by Jason Williams on 2015-08-11.
+//  Copyright (c) 2015 Screaming Goat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SGAppDelegate class]));
+    }
+}
